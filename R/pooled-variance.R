c204f085b44c3cb48038585c250bed24c85ca795
Andrew Gene Brown <brown.andrewg@gmail.com>
2024-02-17 23:23:32 -0800

    Add binomial variance methods

---
+++ R/pooled-variance.R
@@ -0,0 +1,99 @@
+#' Calculate pooled variance for related binomial experiments with confidence intervals
+#'
+#' This method calculates pooled variance for related binomial experiments with confidence intervals. It relies on the normal approximation / Central Limit Theorem to develop a confidence interval each binomial probability in the vector `p_values`. In order for this assumption to work well, `n` should be large. The method adjusts the calculated binomial variance to account for the normal approximation confidence interval, and then calculates pooled variance across all experiments.
+#'
+#'
+#' @param n integer. Number of trials per experiment
+#' @param p_values numeric. Probability of success for each experiment.
+#' @param alpha numeric. Probability level for Z-value in normal distribution confidence interval calculation.
+#'
+#' @export
+#' @examples
+#' n <- 10
+#' p_values <- c(0.2, 0.3, 0.4)
+#'
+#' BinomVarNorm(n, p_values)
+BinomVarNorm <- function(n, p_values, alpha = 0.05) {
+  k <- length(p_values)
+
+  # Calculate variances
+  variances <- sapply(p_values, function(p) n * p * (1 - p))
+
+  # Calculate confidence intervals for each p
+  z_value <- qnorm(1 - alpha / 2)  # Z-value for normal distribution
+  ci <- sapply(p_values, function(p) z_value * sqrt(p * (1 - p) / n))
+
+  # Adjust variances for confidence intervals
+  adjusted_variances <- variances + ci^2
+
+  # Initialize total variance
+  total_variance <- sum(adjusted_variances)
+
+  if (length(adjusted_variances) > 1) {
+    # Calculate covariances and update total variance
+    for (i in 1:(k - 1)) {
+      for (j in (i + 1):k) {
+        cov_ij <-
+          2 * n * p_values[i] * (1 - p_values[i]) * p_values[j] * (1 - p_values[j])
+        total_variance <- total_variance + cov_ij
+      }
+    }
+  }
+  return(total_variance)
+}
+
+#' Calculate pooled variance for related binomial experiments with given confidence intervals
+#'
+#' This method calculates pooled variance for related binomial experiments with confidence intervals. Instead of relying on the normal approximation, the user can calculate or estimate their own confidence intervals. The method adjusts the calculated binomial variance to account for an arbitrary given confidence interval, and then calculates pooled variance across all experiments.
+#'
+#' @param n integer. Number of trials per experiment
+#' @param p_values numeric. Probability of success for each experiment.
+#' @param ci_intervals list of numeric vectors. The list has length equal to length of `p_values`. Each element of the list contains a numeric vector of length 2 denoting the lower and upper confidence limits.
+#' @export
+#'
+#' @examples
+#'
+#' n <- 10  # Number of trials for each experiment
+#'
+#' p_values <- c(0.2, 0.3, 0.4) # Probability of success for each experiment
+#'
+#' # Wilson score intervals for each p
+#' wilson_intervals <- list(
+#'   c(0.1, 0.3),
+#'   c(0.2, 0.4),
+#'   c(0.3, 0.5)
+#' )
+#'
+#' # Calculate pooled variance with custom confidence intervals
+#' BinomVarCustom(n, p_values, wilson_intervals)
+BinomVarCustom <- function(n, p_values, ci_intervals) {
+  k <- length(p_values)
+
+  if (length(ci_intervals) != k) {
+    stop("Number of confidence intervals must match the number of p_values.")
+  }
+
+  # Adjust variances for custom confidence intervals
+  adjusted_variances <- sapply(seq_along(p_values), function(i) {
+    ci <- ci_intervals[[i]]
+    ci_lower <- ci[1]
+    ci_upper <- ci[2]
+    p_hat <- p_values[i]
+    (n * p_hat * (1 - p_hat)) + (ci_upper - p_hat)^2 / 4 + (p_hat - ci_lower)^2 / 4
+  })
+
+  # Initialize total variance
+  total_variance <- sum(adjusted_variances)
+
+  if (length(adjusted_variances) > 1) {
+    # Calculate covariances and update total variance
+    for (i in 1:(k - 1)) {
+      for (j in (i + 1):k) {
+        cov_ij <-
+          2 * n * p_values[i] * (1 - p_values[i]) * p_values[j] * (1 - p_values[j])
+        total_variance <- total_variance + cov_ij
+      }
+    }
+  }
+  return(total_variance)
+}
---
+++ misc/transect-theory-binom-multinom.R
@@ -0,0 +1,50 @@
+library(DescTools)
+
+# 30-point transect
+x30 <- BinomCI(1:30, 30, conf.level = 0.8)
+plot(1:30, x30[, 1], xlim = c(0, 30), ylim = c(0, 1))
+lines(1:30, x30[, 2])
+lines(1:30, x30[, 3])
+
+# 10-point transect
+x10 <- BinomCI(1:10, 10, conf.level = 0.8)
+points(1:10, x10[, 1], xlim = c(0, 10), ylim = c(0, 1))
+lines(1:10, x10[, 2])
+lines(1:10, x10[, 3])
+
+# 5-point transect
+x5 <- BinomCI(1:5, 5, conf.level = 0.8)
+points(1:5, x5[, 1], xlim = c(0, 5), ylim = c(0, 1))
+lines(1:5, x5[, 2])
+lines(1:5, x5[, 3])
+
+# compare CI-width for 5, 10, 30 point transect
+plot(1:30, x30[, 3] - x30[, 2],
+     xlim = c(0, 30), ylim = c(0, 1),
+     xlab = "Number of Stops",
+     ylab = "80% Confidence Interval Width")
+points(1:10, x10[, 3] - x10[, 2], pch = 2)
+points(1:5, x5[, 3] - x5[, 2], pch = 3)
+legend("topright",
+       legend = c("n=30", "n=10", "n=5"),
+       pch = 1:3,)
+
+## multinomial (>2 classes) vs binomial
+
+# complex 2 major + 2 minor
+MultinomCI(c(5, 3, 1, 1), conf.level = 0.8)
+BinomCI(c(5, 3, 1, 1), 10, conf.level = 0.8)
+
+# complex 3 major
+MultinomCI(c(4, 3, 2, 1), conf.level = 0.8)
+BinomCI(c(4, 3, 2, 1), 10, conf.level = 0.8)
+
+# consociation
+MultinomCI(c(8, 1, 1), conf.level = 0.8)
+BinomCI(c(8,1,1), 10, conf.level = 0.8)
+
+# binom diff
+BinomRatioCI(8, 10, 2, 10)
+
+# worst case: number of samples needed to have composition CI width of 10% w/ 80% confidence
+BinomCIn(p = 0.5, 0.10, conf.level = 0.8)
