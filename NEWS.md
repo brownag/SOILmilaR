85587e13ea14511b369404d4233fc161c8e389c6
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-08-19 17:04:35 -0700

    SOILmilaR v0.1

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# SOILmilaR 0.1.0
+
+* Deprecate `thresh` argument, replaced with `thresh_single` (maximum difference in one property) and `thresh_all` (sum of differences across all properties)
+
+* Threshold logic now uses `<` instead of `<=` to be more consistent with many similar soils guides
--- R/similar_soils.R
+++ R/similar_soils.R
@@ -1,34 +1,60 @@
 #' Similar Soil Contrasts
 #'
-#' A method for applying standardized, customizable "similar soils" rules to site-level data derived from various sources.
+#' A method for applying standardized, customizable "similar soils" rules to
+#' site-level data derived from various sources.
 #'
 #' @param x A _data.frame_ or a _SoilProfileCollection_
-#' @param mapping A named _list_ of functions. List element names refer to site-level data columns of `x`. Each specified function provides a conversion of the data element in `x` to a value used in the "similar soils" calculation.
-#' @param condition integer or character. Default: `NULL` the value is calculated internally based on the dominant condition of intersection of mapping results in `x`. You may specify an integer row ID in `x` for specific similar soil contrasts, or you can specify a character dominant condition equivalent to the name assigned by `interaction()` e.g. `"4.3"` for a two rating mapping result where the first rating has value `4` and the second has value `3`
+#' @param mapping A named _list_ of functions. List element names refer to
+#'   site-level data columns of `x`. Each specified function provides a
+#'   conversion of the data element in `x` to a value used in the "similar
+#'   soils" calculation.
+#' @param condition integer or character. Default: `NULL` the value is
+#'   calculated internally based on the dominant condition of intersection of
+#'   mapping results in `x`. You may specify an integer row ID in `x` for
+#' specific similar soil contrasts, or you can specify a character dominant
+#' condition equivalent to the name assigned by `interaction()` e.g. `"4.3"` for
+#' a two rating mapping result where the first rating has value `4` and the
+#' second has value `3`
 #' @param idname ID column name, default `"id"`
-#' @param thresh Sum of differences relative to dominant condition in `x`. Default `1`. See details for discussion on the default calculation for similarity.
-#' @param absolute logical. Report absolute difference? Default: `TRUE`. Absolute difference is always used for comparison against thresh.
+#' @param thresh Deprecated. If used passed to `thresh_all`.
+#' @param thresh_single Sum of differences relative to dominant condition in
+#'   `x`. Default `1`. See details for discussion on the default calculation for
+#'   similarity.
+#' @param thresh_all Sum of differences relative to dominant condition in `x`.
+#'   Default `1`. See details for discussion on the default calculation for
+#'   similarity.
+#' @param absolute logical. Report absolute difference? Default: `TRUE`.
+#'   Absolute difference is always used for comparison against thresh.
 #' @param verbose Default: `TRUE` message about selected `condition`
 #' @details
 #'
-#' The sum of differences across conditions (specified by the intersection of output of the functions in `mapping`) is used as the "distance" of a soil relative to a dominant (or otherwise specified) condition. A threshold value is used to decide which are "similar" and which are not. The functions in mapping can be customized to use alternate thresholds.
+#' The sum of differences across conditions (specified by the intersection of
+#' output of the functions in `mapping`) is used as the "distance" of a soil
+#' relative to a dominant (or otherwise specified) condition. A threshold value
+#' is used to decide which are "similar" and which are not. The functions in
+#' mapping can be customized to use alternate thresholds.
 #'
-#' @return A _data.frame_ containing inputs and two new columns: `similar_dist` (cumulative sum of differences relative to dominant condition), `similar` (logical)
+#' @return A _data.frame_ containing inputs and three new columns:
+#'   `similar_single` (maximum difference in any one property, relative to
+#'   `condition`), `similar_dist` (cumulative sum of differences relative to
+#'   `condition`), `similar` (logical; soil is similar to `condition`)
 #' @export
 #'
-#' @references Norfleet, M.L. and Eppinette, R.T. (1993), A Mathematical Model for Determining Similar and Contrasting Inclusions for Map Unit Descriptons. Soil Survey Horizons, 34: 4-5. <https://doi.org/10.2136/sh1993.1.0004>
+#' @references Norfleet, M.L. and Eppinette, R.T. (1993), A Mathematical Model
+#'   for Determining Similar and Contrasting Inclusions for Map Unit
+#'   Descriptons. Soil Survey Horizons, 34: 4-5.
+#'   <https://doi.org/10.2136/sh1993.1.0004>
 #'
 #' @examplesIf !inherits(try(requireNamespace("aqp", quietly=TRUE) && requireNamespace("dplyr", quietly=TRUE) && requireNamespace("cluster", quietly=TRUE), silent=TRUE), 'try-error')
+#'
 #' set.seed(456)
 #'
-#' x <- do.call('rbind', lapply(1:3, \(i) data.frame(id = paste0(LETTERS[1:10], i),
-#'   taxpartsize = c("fine-loamy", "loamy", "fine-loamy", "fine-loamy",
-#'                   "coarse-loamy", "coarse-loamy", "coarse-loamy", "loamy-skeletal",
-#'                   "loamy-skeletal", "loamy-skeletal"),
-#'   depth = runif(10, 35, 150),
-#'   pscs_clay = c(runif(4, 18, 35), runif(6, 14, 18)),
-#'   pscs_frags = c(runif(3, 0, 15), runif(4, 10, 34),
-#'                  runif(3, 35, 60) + c(0, 40, 0)))))
+#' x <- do.call('rbind', lapply(1:3, \(i) data.frame(id =
+#'   paste0(LETTERS[1:10], i), taxpartsize = c("fine-loamy", "loamy",
+#'   "fine-loamy", "fine-loamy", "coarse-loamy", "coarse-loamy", "coarse-loamy",
+#'   "loamy-skeletal", "loamy-skeletal", "loamy-skeletal"), depth = runif(10,
+#'   35, 150), pscs_clay = c(runif(4, 18, 35), runif(6, 14, 18)), pscs_frags =
+#'   c(runif(3, 0, 15), runif(4, 10, 34), runif(3, 35, 60) + c(0, 40, 0)))))
 #'
 #' rate_taxpartsize <- function(x) {
 #'   # TODO: this is just made up logic for this example and needs to be updated
@@ -42,57 +68,55 @@
 #'                     c("loamy-skeletal", "clayey-skeletal") ~ 7)
 #' }
 #'
-#' rate_depthclass <- function(x,
-#'                             breaks = c(
-#'                               `very shallow` = 25,
-#'                               `shallow` = 50,
-#'                               `moderately deep` = 100,
-#'                               `deep` = 150,
-#'                               `very deep` = 1e4
-#'                             ),
-#'                             pattern = "R|Cr|Cd|kk|m",
-#'                             hzdesgn = aqp::guessHzDesgnName(x, required = TRUE),
+#'   rate_depthclass <- function(x, breaks = c( `very shallow` = 25, `shallow` =
+#'   50, `moderately deep` = 100, `deep` = 150, `very deep` = 1e4 ), pattern =
+#'   "R|Cr|Cd|kk|m", hzdesgn = aqp::guessHzDesgnName(x, required = TRUE),
 #'                             ...) {
 #'   res <- cut(x, c(0, breaks))
 #'   factor(res, levels = levels(res), labels = names(breaks), ordered = TRUE)
 #' }
 #'
-#' rate_pscs_clay <- function(x,
+#'   rate_pscs_clay <- function(x,
 #'                            breaks = c(18, 27, 40, 60, 100)) {
 #'   res <- cut(x, c(0, breaks))
 #'   factor(res, levels = levels(res), ordered = TRUE)
 #' }
 #'
-#' m <- list(taxpartsize = rate_taxpartsize,
-#'           depth = rate_depthclass,
-#'           pscs_clay = rate_pscs_clay)
+#'   m <- list(taxpartsize = rate_taxpartsize, depth = rate_depthclass,
+#'   pscs_clay = rate_pscs_clay)
 #'
-#' s <- similar_soils(x, m)
-#' head(s)
+#'   s <- similar_soils(x, m) head(s)
 #'
-#' # inspect distances using agglomerative clustering+dendrogram
-#' d <- cluster::agnes(s[, 5, drop = FALSE], method="gaverage")
-#' d$height <- d$height + 0.2 # fudge factor for 0-distance
-#' plot(stats::as.dendrogram(d), center=TRUE, type="triangle")
+#'   # inspect distances using agglomerative clustering+dendrogram d <-
+#'   cluster::agnes(s[, 5, drop = FALSE], method="gaverage") d$height <-
+#'   d$height + 0.2 # fudge factor for 0-distance plot(stats::as.dendrogram(d),
+#'   center=TRUE, type="triangle")
 #'
-#' # allow relative contrast ratings to be negative
-#' # (i.e. ordinal factors, concept of "limiting")
-#' # absolute value is still used for "similar" threshold
-#' s2 <- similar_soils(x, m, absolute=FALSE)
+#'   # allow relative contrast ratings to be negative # (i.e. ordinal factors,
+#'   concept of "limiting") # absolute value is still used for "similar"
+#'   threshold s2 <- similar_soils(x, m, absolute=FALSE)
 #'
-#' # inspect distances unsing agglomerative clustering+dendrogram
-#' d2 <- cluster::agnes(s2[, 5, drop = FALSE], method="gaverage")
-#' d2$height <- d2$height + 0.2 # fudge factor for 0-distance
-#' plot(stats::as.dendrogram(d2), center=TRUE, type="triangle")
+#'   # inspect distances unsing agglomerative clustering+dendrogram d2 <-
+#'   cluster::agnes(s2[, 5, drop = FALSE], method="gaverage") d2$height <-
+#'   d2$height + 0.2 # fudge factor for 0-distance
+#'   plot(stats::as.dendrogram(d2), center=TRUE, type="triangle")
 similar_soils <- function(
    x,
    mapping,
    condition = NULL,
    idname = "id",
-   thresh = 1L,
+   thresh = NULL,
+   thresh_single = 2,
+   thresh_all = 3,
    absolute = TRUE,
    verbose = TRUE
  ) {
+
+  if (!missing(thresh)) {
+    .Deprecated(msg = "`thresh` argument has been split into `thresh_single` and `thresh_all`, please use one or both instead. Passing `thresh_all=thresh` and `thresh_single=thresh` for backward compatibility.")
+    thresh_all <- thresh
+    thresh_single <- thresh
+  }
 
   # currently only site-level attributes of a SoilProfileCollection are considered
   if (inherits(x, 'SoilProfileCollection') &&
@@ -146,8 +170,11 @@ similar_soils <- function(
   })
 
   r$similar_dist <- 0
-  r$similar_dist[lex] <- rowSums(ex)
-  r$similar <- abs(r$similar_dist) <= thresh
+  r$similar_single <- 0
+
+  r$similar_single[which(lex)] <- apply(ex, MARGIN = 1, max)
+  r$similar_dist[which(lex)] <- rowSums(ex)
+  r$similar <- ((abs(r$similar_dist) < thresh_all) & (abs(r$similar_single) < thresh_single))
   r <- data.frame(x[idname], r)
 
   r
